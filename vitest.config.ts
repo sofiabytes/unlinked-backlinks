9d38ca790940edabef5682f4f8efb719d1ef06eb
Sofiabytes <dev@sofiabytes.net>
2026-03-06 20:08:34 +0000

    Fix test version

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vitest/config';
+import * as path from 'path';
 
 export default defineConfig({
   test: {
     environment: 'node',
     alias: {
-      'obsidian': './tests/obsidian-mock.ts'
+      'obsidian': path.resolve(__dirname, './tests/obsidian-mock.ts')
     }
   }
 });
